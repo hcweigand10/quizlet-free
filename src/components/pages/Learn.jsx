7f68989ca4f3bbe13a7e122b65b4239c274ff446
hcweigand10 <henryweigand10@gmail.com>
2023-09-26 17:03:37 -0700

    learning close..

--- src/components/pages/Learn.jsx
+++ src/components/pages/Learn.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client";
 import React, { useState, useEffect } from "react";
 import { DECK } from "../../utils/queries";
 import Modal from "../UI/Modal";
+import Button from "../UI/Button";
 import Loading from "../UI/Loading";
 
 const Learn = () => {
@@ -9,13 +10,15 @@ const Learn = () => {
   const [levelOne, setLevelOne] = useState([]);
   const [levelTwo, setLevelTwo] = useState([]);
   const [levelThree, setLevelThree] = useState([]);
-  const [current, setCurrent] = useState({});
+  const [current, setCurrent] = useState({ prompt: "", answer: "", level: "" });
   const [guess, setGuess] = useState("");
   const [fix, setFix] = useState("");
   const [status, setStatus] = useState("");
+  const [fixStatus, setFixStatus] = useState("");
   const [visible, setVisible] = useState(false);
   const [rounds, setRounds] = useState(0);
   const [correct, setCorrect] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
 
   const deckId = window.location.pathname.split("/")[2];
 
@@ -25,9 +28,21 @@ const Learn = () => {
     if (data) {
       const shufffled = shuffleArray(data.deck.cards);
       setCurrent({ ...shufffled[0], level: "unseen" });
-      setUnseen(shufffled.slice(1));
+      setUnseen(shufffled);
     }
   }, [data]);
+
+  useEffect(() => {
+    if (levelThree.length === data?.deck.cards.length) {
+      setRounds(rounds-1)
+      endGame();
+    }
+  }, [levelThree]);
+
+  useEffect(() => {
+    nextCard()
+  }, [rounds])
+  
 
   function shuffleArray(array) {
     const result = [...array];
@@ -53,32 +68,34 @@ const Learn = () => {
   const correctAnswer = () => {
     setStatus("correct!");
     // if first time seeing, move to level 3
-    if (current.level === "unseen") {
-      setLevelThree([...levelThree, current]);
-      // else move it up one level
-    } else if (levelOne.includes(current)) {
+    if (current.level === "levelOne") {
+      console.log("hello");
+      setLevelOne(levelOne.slice(1));
       setLevelTwo([...levelTwo, current]);
-    } else if (levelTwo.includes(current)) {
+    } else if (current.level === "levelTwo") {
+      setLevelTwo(levelTwo.slice(1));
+      setLevelThree([...levelThree, current]);
+    } else if (current.level === "unseen") {
+      setUnseen(unseen.slice(1));
       setLevelThree([...levelThree, current]);
     }
     setCorrect(correct + 1);
-    setRounds(rounds + 1);
-    if (levelThree.length === data.deck.cards - 1) {
-      endGame();
-    } else {
-      nextCard();
-      setTimeout(() => {
-        setStatus("");
-      }, 2000);
-    }
+    setRounds(rounds+1)
+    setTimeout(() => {
+      setStatus("");
+    }, 1000);
   };
 
   const wrongAnswer = () => {
-    console.log(guess);
     //add to level one
-    setLevelOne([...levelOne, current]);
+    if (current.level === "levelTwo") {
+      setLevelTwo(levelTwo.slice(1));
+      setLevelOne([...levelOne, current]);
+    } else if (current.level === "unseen") {
+      setUnseen(unseen.slice(1));
+      setLevelOne([...levelOne, current]);
+    }
     //remove from previous level
-    setRounds(rounds + 1);
     setVisible(true);
   };
 
@@ -88,43 +105,52 @@ const Learn = () => {
 
   const nextCard = () => {
     setGuess("");
-    if (levelThree.length === data.deck.cards) {
-      return endGame();
-    }
     // if still unseen cards, use those
     if (unseen.length > 0) {
-      console.log("here")
       setCurrent({ ...unseen[0], level: "unseen" });
-      setUnseen(unseen.slice(1));
-    } else if (levelOne.length > 0 && levelTwo.length > 0) { // if both level one and two, coin flip to use one of them
+      // setUnseen(unseen.slice(1));
+    } else if (levelOne.length > 0 && levelTwo.length > 0) {
+      // if both level one and two, coin flip to use one of them
       const flip = coinFlip();
-      if (flip) {
+      if (current.prompt === levelOne[0].prompt) {
+        setCurrent({ ...levelTwo[0], level: "levelTwo" });
+      } else if (current.prompt === levelTwo[0].prompt) {
+        setCurrent({ ...levelOne[0], level: "levelTwo" });
+      }
+      else if (flip) {
         setCurrent({ ...levelOne[0], level: "levelOne" });
-        setLevelOne(levelOne.slice(1));
+        // setLevelOne(levelOne.slice(1));
       } else {
         setCurrent({ ...levelTwo[0], level: "levelTwo" });
-        setLevelTwo(levelTwo.slice(1));
+        // setLevelTwo(levelTwo.slice(1));
       }
-    } else { // if only one remaining, use that
+    } else {
+      // if only one remaining, use that
       if (levelOne.length === 0) {
         setCurrent({ ...levelTwo[0], level: "levelTwo" });
-        setLevelTwo(levelTwo.slice(1));
+        // setLevelTwo(levelTwo.slice(1));
       } else {
         setCurrent({ ...levelOne[0], level: "levelOne" });
-        setLevelOne(levelOne.slice(1));
+        // setLevelOne(levelOne.slice(1));
       }
     }
   };
 
   const checkFix = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const correct = current.answer.split("(")[0].trim().toLowerCase();
     if (fix === correct) {
-      
+      setVisible(false);
+      setFix("");
+      setRounds(rounds+1)
+    } else {
+      setFixStatus("Not quite... check your spelling");
     }
-  }
+  };
 
-  const endGame = () => {};
+  const endGame = () => {
+    setGameOver(true);
+  };
 
   const coinFlip = () => Math.random() > 0.5;
 
@@ -149,22 +175,28 @@ const Learn = () => {
           <h4>Almost: {levelTwo.length}</h4>
           <h4>Locked down: {levelThree.length}</h4>
         </div>
-        <div className="col-span-1 md:col-span-3 bg-white rounded p-5">
-          <h4 className="text-slate-600">Prompt</h4>
-          <h2 className="text-3xl">{current.prompt}</h2>
-          <form className="mt-5" onSubmit={check}>
-            <label className="text-sm text-slate-600 pb-1 block">
-              Answer here
-            </label>
-            <input
-              type="text"
-              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-80"
-              name="guess"
-              value={guess}
-              onChange={handleInputChange}
-            />
-          </form>
-        </div>
+        {gameOver ? (
+          <div className="col-span-1 md:col-span-3 bg-white rounded p-5">
+            Nicely done!
+          </div>
+        ) : (
+          <div className="col-span-1 md:col-span-3 bg-white rounded p-5">
+            <h4 className="text-slate-600">Prompt</h4>
+            <h2 className="text-3xl">{current.prompt}</h2>
+            <form className="mt-5" onSubmit={check}>
+              <label className="text-sm text-slate-600 pb-1 block">
+                Answer here
+              </label>
+              <input
+                type="text"
+                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-80"
+                name="guess"
+                value={guess}
+                onChange={handleInputChange}
+              />
+            </form>
+          </div>
+        )}
         <div className="col-span-1 bg-white rounded p-5">
           <h4>Round: {rounds + 1}</h4>
           <h4>Percentage: {rounds ? (100 * correct) / rounds : "-"}%</h4>
@@ -180,17 +212,28 @@ const Learn = () => {
           <span className="text-slate-600 italic">{current.answer}</span>
         </h2>
         <form onSubmit={checkFix}>
-        <label className="text-sm text-slate-600 pb-1 block">
-              Answer here
-            </label>
-            <input
-              type="text"
-              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-80"
-              name="fix"
-              value={fix}
-              onChange={handleInputChange}
-            />
+          <label className="text-sm text-slate-600 py-1 block">
+            Type it out here to continue
+          </label>
+          <input
+            type="text"
+            className="border rounded-lg px-3 py-2 mt-1 mb-3 text-sm w-80"
+            name="fix"
+            value={fix}
+            onChange={(e) => setFix(e.target.value)}
+          />
+          <br />
+          <Button
+            className={"mx-2"}
+            type={"submit"}
+            disabled={
+              fix !== current?.answer?.split("(")[0].trim().toLowerCase()
+            }
+          >
+            Got it
+          </Button>
         </form>
+        <h3 className="italic text-xl">{fixStatus}</h3>
       </Modal>
     </div>
   );
