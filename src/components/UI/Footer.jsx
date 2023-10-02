347424425116e9556232a367b496b9678e5666d0
hcweigand10 <henryweigand10@gmail.com>
2023-10-02 15:10:26 -0700

    footer

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,12 +1,29 @@
 import React, {useState} from 'react'
+import icon from "../../../public/icon.png"
 
 const Footer = () => {
 
     return (
-        <div className='h-40 bg-slate-200 text-slate-600'>
-             <h1>Footer</h1>
-             {/* <a href="https://www.flaticon.com/free-icons/elephant" title="elephant icons">Elephant icons created by Freepik - Flaticon</a> */}
+            
+<footer class="bg-slate-200 shadow dark:bg-gray-900 w-full">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+                <img src={icon} class="h-8 mr-3" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Quiz Quo Pro</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+            </ul>
         </div>
+        <hr class="my-6 border-gray-300 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://henryweigand.com/" class="underline" target='_blank' rel='noreferrer'>henryweigand.com</a></span>
+    </div>
+</footer>
+
+
     )
 }
 
